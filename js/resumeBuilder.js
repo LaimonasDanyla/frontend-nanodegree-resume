3c39001b0e6a08b392389d33bb04a49252db5a71
Laimonas Danyla <laimonas.danyla@gmail.com>
2016-01-12 18:16:39 +0200

    Add location, dates, description

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -30,13 +30,28 @@ var work = {
 
 for (job in work.jobs) {
   $("#workExperience").append(HTMLworkStart);
+
   var formattedEmployer = HTMLworkEmployer.replace
-  ("%data%", work.jobs[job].employer);
-  var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+   ("%data%", work.jobs[job].employer);
+
+  var formattedTitle = HTMLworkTitle.replace("%data%",
+   work.jobs[job].title);
+
   var formattedEmployerTitle = formattedEmployer + formattedTitle;
   $(".work-entry:last").append(formattedEmployerTitle);
-}
 
+  var formattedLocation = HTMLworkLocation.replace("%data%",
+   work.jobs[job].location);
+  $(".work-entry:last").append(formattedLocation);
+
+  var formattedDates = HTMLworkDates.replace("%data%",
+   work.jobs[job].dates);
+  $(".work-entry:last").append(formattedDates);
+
+  var formattedDescription = HTMLworkDescription.replace("%data%",
+   work.jobs[job].description);
+  $(".work-entry:last").append(formattedDescription);
+}
 
 var projects = {
   "project": [
